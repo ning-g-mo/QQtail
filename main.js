7acb7977e43620c8115db1be67bd9f3d8c7e560c
ning-g-m <3436464181@qq.com>
2025-07-03 09:58:22 +0800

    初始

---
+++ main.js
@@ -0,0 +1,16 @@
+/**引入库 */
+import qtail from 'https://cdn.jsdelivr.net/npm/qtail-js@latest/+esm';
+/**初始化 */
+const a = new qtail("","");
+/**获取值 */
+document.getElementById('qz').value
+document.getElementById('wb').value
+a.qtail.update("qz");
+a.qtail.upd("wb");
+
+/**输出值 */
+
+qtail.generate(a); {
+     sc.value = a.qtail.get()
+}
+qtail.gen(a)
