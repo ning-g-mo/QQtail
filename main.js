371423269f7db5f324d51e3730660a307b0f00b2
ning-g-m <3436464181@qq.com>
2025-07-09 01:58:21 +0800

    refactor(index.html): 移除复制按钮的 onclick 属性
    - 从 index.html 中移除了复制按钮的 onclick 属性
    - 在 main.js 中添加了复制功能的实现代码
    - 优化了 main.js 中的逻辑，增加了事件监听器和输入事件处理

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-/**引入库 */
-import qtail from 'https://cdn.jsdelivr.net/npm/qtail-js@latest/+esm';
-/**初始化 */
-const a = new qtail("","");
-/**获取值 */
-document.getElementById('qz').value
-document.getElementById('wb').value
-a.qtail.update("qz");
-a.qtail.upd("wb");
+/**引入库, 生产环境建议用指定版本而不是latest */
+import Qtail from 'https://cdn.jsdelivr.net/npm/qtail-js@latest/+esm';
+// 类名首字母应该大写(
 
-/**输出值 */
+const qtail = new Qtail( " ", " " ,true);
+// HTML里调用的一般都是全局变量，所以应该在这里声明
 
-qtail.generate(a); {
-     sc.value = a.qtail.get()
+// 注册事件监听器，load 为加载完毕事件
+// TIP: 建议用addEventListener，但是我觉得这样你更好理解
+window.onload = function(){
+  // 这里是整页面(window) 加载完毕
+  // 从ID获取元素(页面内容)
+  var nick = document.getElementById( 'qz' ); // 通过id获取元素
+  var tail = document.getElementById( 'wb' );
+  var output = document.getElementById( 'sc' );
+  // 注册输入事件 input
+  nick.oninput = tail.oninput = function(){
+    // 通过value获取input元素值
+    qtail.update( nick.value, tail.value );
+    // textContent文本内容 innerHTML超文本内容
+    output.textContent = qtail.generate();
+  }
+
+  // 如果你用mjs的话就不要用HTML绑定事件了，因为mjs是异步加载的
+  var copy = document.getElementById( 'copy' );
+  copy.onclick = function(){
+    // 调用浏览器复制Api, 非本地需要https，有需要可以用Openssl自签一个
+    // 这个属实是有点超纲了，你连Promise都不清楚让你写这个属于是(难说)
+    navigator.clipboard.writeText( output.textContent ).then(() => {
+      // then是如果成功执行的函数
+      // 箭头函数多行要用{}，用return返回值
+      alert( '复制成功！' );
+      // 弹窗，这个你应该会，我就不细讲了
+    })
+  }
 }
-qtail.gen(a)
